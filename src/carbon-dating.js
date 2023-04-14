d3382c785529159fa215f99176698357384446bd
Stanislav Cherevkov <web--dev@hotmail.com>
2023-04-14 22:23:32 +0300

    feat: add 'Carbon dating' solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let age = false;
+
+  if (typeof sampleActivity === 'string') {
+    sampleActivity = Number(sampleActivity);
+
+    if (typeof sampleActivity === 'number' && !isNaN(sampleActivity) && sampleActivity != false && sampleActivity > 0 && sampleActivity <= 15) {
+      const difActivity = Math.log(MODERN_ACTIVITY/sampleActivity);
+      const k = 0.693/HALF_LIFE_PERIOD;
+      age = Math.ceil(difActivity / k);
+    }
+  } 
+
+  return age;
 }
 
 module.exports = {
